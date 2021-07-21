670ae296d24301f16c9d1a2d2a19f7fce0f94aad
Akash Takawale <atakawale207@gmail.com>
2021-07-21 19:22:35 +0530

    Initial setup with eslint

---
+++ index.js
@@ -0,0 +1,5 @@
+const profile = require('./profile')
+
+console.log(
+    profile('John Doe','johndoe@gmail.com',24)
+)
---
+++ profile.js
@@ -0,0 +1,5 @@
+const profile = (name,email,age) => {
+    return `My name is ${name} and I am ${age} years old. You can mail at ${email}`
+}
+
+module.exports = profile
