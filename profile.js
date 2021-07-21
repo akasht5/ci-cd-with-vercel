493790274bdb6695ad7a379dd98e7fdf3c8fce21
Akash Takawale <atakawale207@gmail.com>
2021-07-21 22:12:01 +0530

    fix linting

--- profile.js
+++ profile.js
@@ -1,5 +1,3 @@
-const profile = (name,email,age) => {
-    return `My name is ${name} and I am ${age} years old. You can mail at ${email}`
-}
+const profile = (name, email, age) => `My name is ${name} and I am ${age} years old. You can mail at ${email}`;
 
-module.exports = profile
+module.exports = profile;
