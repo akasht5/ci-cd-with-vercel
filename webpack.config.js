2a3c1c964d6b2e5d1b5523cc3b6ad16f497bf286
Akash Takawale <atakawale207@gmail.com>
2021-07-29 19:04:32 +0530

    testing a failure in build task

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
+        excludes: /node_modules/,
+        uses: 'babel-loader',
       },
     ],
   },
